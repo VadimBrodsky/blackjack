1b8ead7acab60e413a0783a78a8f063bd07d9d7c
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-05-18 22:11:41 -0400

    Revise data structure, created deck

--- blackjack.rb
+++ blackjack.rb
@@ -3,9 +3,13 @@
 require 'pry'
 require_relative 'lib/blackjack_methods'
 
-CARDS = %w(2 3 4 5 6 7 8 9 10 Jack Queen King Ace)
+CARDS = { '2' => 2, '3' => 3, '4' => 4, '5' => 5,
+          '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10,
+          'jack' => 10, 'queen' => 10, 'king' => 10, 'ace' => [1, 10] }
 SUITS = { 'spades' => '♠', 'hearts' => '♥', 'diamonds' => '♦', 'clubs' => '♣' }
+DECK = CARDS.keys.product(SUITS.keys)
 
 puts 'Welcome to BlackJack!'
-p CARDS
-p SUITS
+# p CARDS
+# p SUITS
+p DECK.count
